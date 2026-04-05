df5ad7649f4792eaa8f1b23c774cfdfc9e3bd424
pablo <pabloeduardomenendez@gmail.com>
2026-04-05 20:31:48 +0200

    WIP prototype CPU execute refactor

--- src/cpu.c
+++ src/cpu.c
@@ -13,11 +13,40 @@ uint8_t cpu_fetch(CPU *cpu){
     return opcode;
 }
 
-void cpu_step(CPU *cpu){
+const InstructionInfo* cpu_decode(CPU *cpu){
     uint8_t opcode = cpu_fetch(cpu);
     const InstructionInfo *instruction = cpu_decode(opcode);
+    return instruction;
+}
 
-    if (instruction->handler != NULL) {
-        instruction->handler(cpu);
+void cpu_execute(CPU *cpu, ADDRESS_MODE address_mode, InstructionHandler* handler){
+    switch(address_mode)
+    {
+        case ACCUMULATOR:
+            (*handler)(cpu);
+            break;
+        case ABSOLUTE:
+            uint8_t address_LL = cpu_fetch(cpu);
+            uint8_t address_HH = cpu_fetch(cpu);
+            uint16_t address = (uint16_t)(address_HH << 8) | address_LL; 
+            uint8_t operand = bus_read(cpu->bus, address);
+            (*handler)(cpu, operand);
+            break;
     }
 }
+
+void cpu_set_flag(CPU *cpu, STATUS_FLAGS flag, bool set){
+    if (set)
+    {
+        cpu->P |= flag;
+    }
+    else 
+    {
+        cpu->P &= ~flag;
+    }
+}
+
+bool cpu_get_flag(const CPU *cpu, STATUS_FLAGS flag){
+    return cpu->P & flag;
+}
+
