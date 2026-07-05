ad3a991a7e07d2e9d92f091f290a12d393625ae2
pablo <pabloeduardomenendez@gmail.com>
2026-07-05 20:40:37 +0200

    bus fixed

--- includes/bus.h
+++ includes/bus.h
@@ -3,13 +3,14 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include "cartridge.h"
 
 typedef struct { 
-    uint8_t data[65536]; 
+    Cartridge* cart; 
+    uint8_t ram[2048]; 
 } Bus;
 
 void bus_write(Bus *bus, uint16_t addr, uint8_t val);
 uint8_t bus_read(const Bus *bus, uint16_t addr);
-bool bus_load(Bus *bus, uint16_t addr, const uint8_t *data, size_t len);
 
 #endif // BUS_H
--- src/bus.c
+++ src/bus.c
@@ -2,19 +2,40 @@
 #include <string.h>
 
 void bus_write(Bus* bus, uint16_t addr, uint8_t val){
-    bus->data[addr] = val;
+    switch (addr)
+    {
+        case 0x0000 ... 0x1FFF:
+            bus->ram[addr & 0x07FF] = val;
+            break;
+        case 0x2000 ... 0x401F:
+            break;
+        case 0x4020 ... 0x7FFF:
+            break;
+        case 0x8000 ... 0xFFFF:
+            cartridge_prg_write(bus->cart, addr, val);
+            break;
+        default:
+            break; 
+    }
 }
 
 uint8_t bus_read(const Bus* bus, uint16_t addr){
-    return bus->data[addr];
-}
-
-bool bus_load(Bus *bus, uint16_t addr, const uint8_t *data, size_t len){
-    if ((addr + len) <= 65536){
-        memcpy(bus->data + addr, data, len);
-        return true;
-    }
-    else{
-        return false;
+    switch (addr)
+    {
+        case 0x0000 ... 0x1FFF:
+            return bus->ram[addr & 0x07FF];
+            break;
+        case 0x2000 ... 0x401F:
+            return 0;
+            break;
+        case 0x4020 ... 0x7FFF:
+            return 0;
+            break;
+        case 0x8000 ... 0xFFFF:
+            return cartridge_prg_read(bus->cart, addr);
+            break;
+        default:
+            return 0;
+            break; 
     }
 }
